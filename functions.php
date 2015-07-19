3b1f0de9099e0032bbcbabb80bad49d3e6e9d0b1
aleksandar <alpe88@users.noreply.github.com>
2015-07-19 16:52:27 -0700

    added the standard description comment
    look @ top of file

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
-
-/* functions.php */
+/*
+Theme Name: Theme built for the Puget Sound Association of The Phi Beta Kappa Honor Society
+Author: Bo LOCKWOOD, CHRIS W, 
+ZHANG, BOQUN, 
+PETROVIC, ALEKSANDAR
+HAILEMARIAM, MAHILET D
+MALAVE, RAFAEL E
+Author URI: 
+Text Domain: PSA-PBK
+Description: This is a theme developed for the Puget Sound Association of The Phi Beta Kappa Honor Society
+Version: 0.0
+*/
 <?php
 
 $args = array(
