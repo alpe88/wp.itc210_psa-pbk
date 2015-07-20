7f458ec0161da099f144c7f070a581314eacef6a
aleksandar <alpe88.dev@gmail.com>
2015-07-19 17:18:12 -0700

    added more useful functions
    seo, bootstrap walker, additional theme support, post type support...

--- functions.php
+++ functions.php
@@ -11,26 +11,31 @@ Description: This is a theme developed for the Puget Sound Association of The Ph
 Version: 0.0
 */
 <?php
+#custom walker include
+require_once('DD_Walker.php');
 
-$args = array(
+#theme support additions
+$theme_args = array(
 	'search-form',
 	'comment-form',
 	'comment-list',
 	'gallery',
-	'caption'
+	'caption',
+	'post-thumbnails'
 );
-add_theme_support( 'html5', $args );
+add_theme_support( 'html5', $theme_args );
+add_post_type_support( 'page', 'excerpt', post-formats );
 
-// add RSS feed links to <head> tag
+#add RSS feed links to <head> tag
 add_theme_support( 'automatic-feed-links' );
 
-//for security, hide wp version in web pages and feeds
+#for security, hide wp version in web pages and feeds
 function remove_version_info() {
      return '';
 }
 add_filter('the_generator', 'remove_version_info');
 
-// set Media Library image link default to "none"
+#set Media Library image link default to "none"
 function wpb_imagelink_setup() {
 	$image_set = get_option( 'image_default_link_type' );
 	
@@ -40,21 +45,22 @@ function wpb_imagelink_setup() {
 }
 add_action('admin_init', 'wpb_imagelink_setup', 10);
 
-// use shortcodes in widgets
+#use shortcodes in widgets
 add_filter( 'widget_text', 'shortcode_unautop');
 add_filter( 'widget_text', 'do_shortcode');
 
-//Register custom menus
+#Register custom menus
 if ( function_exists( 'register_nav_menus' ) ) {
 	register_nav_menus(
 		array(
 		  'main-menu' => 'Main Menu',
 		  'utility-menu' => 'Utility Menu'
+		  'social_menu' => 'Social Menu' 
 		)
 	);
 }   
    
-//Register sidebars
+#Register sidebars
 add_action( 'widgets_init', 'my_register_sidebars' );
 
 function my_register_sidebars() {
@@ -99,8 +105,7 @@ function my_register_sidebars() {
 	/* Repeat register_sidebar() code for additional sidebars. */
 }
 
-
-// Remove rel attribute from the category list
+#Remove rel attribute from the category list
 function remove_category_list_rel($output)
 {
   $output = str_replace(' rel="category tag"', '', $output);
@@ -108,3 +113,32 @@ function remove_category_list_rel($output)
 }
 add_filter('wp_list_categories', 'remove_category_list_rel');
 add_filter('the_category', 'remove_category_list_rel');
+
+#SEO Titling:
+function SEO_title(){
+	global $post;
+
+	if(is_front_page()){
+		echo bloginfo('description');
+	}
+	elseif(is_page() || is_single()){
+		echo the_title();
+	}
+	elseif(is_author()){
+		if ($author_id = get_query_var('author')){$author = get_user_by( 'id',$author_id);}
+		echo $author->first_name . ' ' . $author->last_name;
+	}
+	else{
+		echo bloginfo('description');
+	}
+
+	if($post->post_parent){
+		echo ' | ';
+		echo get_the_title($post->post_parent);
+	}
+	
+	echo ' | ';
+	echo bloginfo('name');
+	echo ' | ';
+	echo 'Seattle, WA';
+}
