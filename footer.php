dba773a25c1566a616400b6ec2fd0ef37cdff7bd
naomi <naomiwatabetaylor@gmail.com>
2015-07-26 21:56:42 -0700

    Revert "completed front page"
    This reverts commit 3ed83f976cdf33d5771ba07f69b2fa9daca78458.

--- footer.php
+++ footer.php
@@ -1,57 +1,50 @@
-<div class="margin-bottom-lg"></div>
-<div class="container well">
+<div class="">
+<hr />
+<div class="container">
 	<div class="row">
-		<div class="col-xs-12">
-				<div class="col-xs-6 text-center nopadding">
-					<div id="social_navigation_footer">
-						<ul class="nopadding">
-							<?php wp_nav_menu(
-								array( 
-									'theme_location' => 'social_menu',
-									'menu'           => 'Social Menu',
-									'container'      => '', 
-									'container_id'   => '',
-									'container_class'=> '',
-									'menu_id'        => '',
-									'menu_class'     => '',
-									'items_wrap'     => '%3$s',
-									'fallback_cb'	 => '',
-									)
-								);
-							?>
-						</ul>
-					</div>
-				</div>
-				<div class="col-xs-6 text-center nopadding b-l">
-					<a id="footer-banner" href="<?php echo home_url(); ?>">ΦΒΚ
-					</a>
-				</div>
-				<div class="col-xs-12 text-center">
-					<a title="Email Us!" href="/contact/">Puget Sound Association of The Phi Beta Kappa Honor Society &nbsp &copy; <?php
-										$copyYear = 2015; #Set your website start date
-										$curYear = date('Y'); #Keeps the second year updated
-										echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
-										?></a>
+			<div class="">
+				<div id="social_navigation_footer" class="">
+					<ul class="nopadding">
+						<?php wp_nav_menu(
+							array( 
+								'theme_location' => 'social_menu',
+								'menu'           => 'Social Menu',
+						 		'container'      => '', 
+						 		'container_id'   => '',
+			 			 		'container_class'=> '',
+								'menu_id'        => '',
+								'menu_class'     => '',
+								'items_wrap'     => '%3$s',
+								'fallback_cb'	 => '',
+								)
+							);
+						?>
+					</ul>
 				</div>
 			</div>
-		</div>
+			<div class="text-center">
+					<p class="tagline"><a title="Email Us!" href="/contact/">Puget Sound Association of The Phi Beta Kappa Honor Society &nbsp &copy; <?php
+									$copyYear = 2015; #Set your website start date
+									$curYear = date('Y'); #Keeps the second year updated
+									echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+									?></a></p>
+			</div>
 	</div>
+</div>
 </div>
 	<!-- Begin Scripts -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
    	<script src="<?php bloginfo('template_directory');?>/js/bootstrap.min.js"></script>
-	<!-- Twitter Fetcher -->
-	<script src="<?php bloginfo('template_directory');?>/tw/tf.min.js"></script>
+   	<script src="<?php bloginfo('template_directory');?>/js/bootswatch.js"></script>
 	<!-- FlexSlider -->
   	<script src="<?php bloginfo('template_directory');?>/flexslider/jquery.flexslider-min.js"></script>
   	<script type="text/javascript">
 		$(document).ready(function() {
 			$('.flexslider').flexslider({
 				animation: "fade",
-				controlNav: true,
-				directionNav: true
+				controlNav: false,
+				directionNav: false
 			});
-		});
 	</script>
 <?php wp_footer(); ?>
 <!-- Footer Ends Here -->
--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 	
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style.css" type="text/css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/bootstrap.min.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/bootswatch.min.css" type="text/css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/flexslider/flexslider.css" type="text/css">
 	<!--[if lt IE 9]>
         	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -31,49 +32,37 @@
 	<!-- Header Ends Here -->
 	<!-- Body Begins Here -->
 	<body <?php body_class(''); ?> >
-	<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
-		<nav id="nav_wrap" class="navbar navbar-default" role="navigation">
-		<a href="<?php echo home_url(); ?>"><img class="margin-bottom-sm center-block img-responsive" src="<?php bloginfo('template_directory');?>/images/psa-pbk-logo.jpg" alt="Puget Sound Association of The Phi Beta Kappa Honor Society - Logo." />
-							</a>
+		<nav id="nav_wrap" class="navbar navbar-default navbar-fixed-top" role="navigation">
 			<div class="container">
-				<div class="row">
-					<div class="col-xs-12">
-					<div class="navbar-header">
-						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-main" >
-								<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="navbar-collapse collapse navbar-main">
-								<ul class="nav navbar-nav" id="main-menu">
-								<?php wp_nav_menu(
-									array( 
-										'theme_location' => 'main_menu',
-										'menu'           => 'Main Menu',
-										'depth'          => '2',
-										'container'      => '', 
-										'container_id'   => '',
-										'container_class'=> '',
-										'menu_id'        => '',
-										'menu_class'     => '',
-										'items_wrap'     => '%3$s',
-										'fallback_cb'	 => '',
-										'walker'         => new DD_Walker(),
-										));
-								?>
-								</ul>
-							</div>
-					</div>
+				<div class="navbar-header">
+					<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-main" >
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+        						<span class="icon-bar"></span>
+        						<span class="icon-bar"></span>
+					</button>
+					<a href="<?php echo home_url(); ?>">
+							<h1 id="logo" class="">PSA - PBK</h1>
+					</a>
 				</div>
+				<div id="main-menu" class="navbar-collapse collapse navbar-main uppercase navbar-right">
+							<ul class="nav navbar-nav">
+							<?php wp_nav_menu(
+								array( 
+									'theme_location' => 'main_menu',
+									'menu'           => 'Main Menu',
+									'depth'          => '2',
+									'container'      => '', 
+									'container_id'   => '',
+									'container_class'=> '',
+									'menu_id'        => '',
+									'menu_class'     => '',
+									'items_wrap'     => '%3$s',
+									'fallback_cb'	 => '',
+									'walker'         => new DD_Walker(),
+									));
+							?>
+							</ul>
+						</div>
 			</div>	
 		</nav>
-		<div class="margin-bottom-lg"></div>
