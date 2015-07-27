b6aaa35c099cabe5abb9c5e8992e8755250c736a
aleksandar <alpe88.dev@gmail.com>
2015-07-26 22:00:40 -0700

    completed front page
    @bzhang10 @cwlock @mahilet @rmalave check it out...

--- footer.php
+++ footer.php
@@ -1,50 +1,57 @@
-<div class="">
-<hr />
-<div class="container">
+<div class="margin-bottom-lg"></div>
+<div class="container well">
 	<div class="row">
-			<div class="">
-				<div id="social_navigation_footer" class="">
-					<ul class="nopadding">
-						<?php wp_nav_menu(
-							array( 
-								'theme_location' => 'social_menu',
-								'menu'           => 'Social Menu',
-						 		'container'      => '', 
-						 		'container_id'   => '',
-			 			 		'container_class'=> '',
-								'menu_id'        => '',
-								'menu_class'     => '',
-								'items_wrap'     => '%3$s',
-								'fallback_cb'	 => '',
-								)
-							);
-						?>
-					</ul>
+		<div class="col-xs-12">
+				<div class="col-xs-6 text-center nopadding">
+					<div id="social_navigation_footer">
+						<ul class="nopadding">
+							<?php wp_nav_menu(
+								array( 
+									'theme_location' => 'social_menu',
+									'menu'           => 'Social Menu',
+									'container'      => '', 
+									'container_id'   => '',
+									'container_class'=> '',
+									'menu_id'        => '',
+									'menu_class'     => '',
+									'items_wrap'     => '%3$s',
+									'fallback_cb'	 => '',
+									)
+								);
+							?>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-6 text-center nopadding b-l">
+					<a id="footer-banner" href="<?php echo home_url(); ?>">ΦΒΚ
+					</a>
+				</div>
+				<div class="col-xs-12 text-center">
+					<a title="Email Us!" href="/contact/">Puget Sound Association of The Phi Beta Kappa Honor Society &nbsp &copy; <?php
+										$copyYear = 2015; #Set your website start date
+										$curYear = date('Y'); #Keeps the second year updated
+										echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+										?></a>
 				</div>
 			</div>
-			<div class="text-center">
-					<p class="tagline"><a title="Email Us!" href="/contact/">Puget Sound Association of The Phi Beta Kappa Honor Society &nbsp &copy; <?php
-									$copyYear = 2015; #Set your website start date
-									$curYear = date('Y'); #Keeps the second year updated
-									echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
-									?></a></p>
-			</div>
+		</div>
 	</div>
-</div>
 </div>
 	<!-- Begin Scripts -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
    	<script src="<?php bloginfo('template_directory');?>/js/bootstrap.min.js"></script>
-   	<script src="<?php bloginfo('template_directory');?>/js/bootswatch.js"></script>
+	<!-- Twitter Fetcher -->
+	<script src="<?php bloginfo('template_directory');?>/tw/tf.min.js"></script>
 	<!-- FlexSlider -->
   	<script src="<?php bloginfo('template_directory');?>/flexslider/jquery.flexslider-min.js"></script>
   	<script type="text/javascript">
 		$(document).ready(function() {
 			$('.flexslider').flexslider({
 				animation: "fade",
-				controlNav: false,
-				directionNav: false
+				controlNav: true,
+				directionNav: true
 			});
+		});
 	</script>
 <?php wp_footer(); ?>
 <!-- Footer Ends Here -->
---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+<!-- Header Ends Here -->
+<?php $args = array(
+								'post_type'              => 'post',
+								'post_status'            => 'publish',
+								'posts_per_page'         => '1',
+								'order'                  => 'DESC',
+								'orderby'                => 'date',
+								'category__in'			 => '5',
+								);
+			$posts_query = new WP_Query($args);	?>
+<!-- Content Begins Here -->
+<!-- Main Content Section Begins Here -->
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 margin-bottom-lg">
+			<?php add_flexslider($posts_query->ID()); ?>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-8">
+			<?php if($posts_query->have_posts()): ?>
+			<!-- the loop -->
+				<?php while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
+					<article id="post-content-<?php the_ID(); ?>" class="post-content">
+						<?php if(has_post_thumbnail()){the_post_thumbnail('',array('class' => "img-thumbnail img-responsive block-center"));} ?>
+								<div class="col-xs-9"><h1><?php the_title(); ?></h1>
+								<?php the_content();?></div>
+					</article>
+			<?php endwhile; ?>
+			<?php wp_reset_postdata();?>
+			<?php else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+		</div>
+		<div class="col-xs-12 col-sm-4">
+			<?php get_sidebar();?>
+		</div>
+	</div>
+</div>
+<!-- Main Content Section Ends Here -->
+<!-- Content Ends Here -->
+
+<!-- Footer Begins Here -->
+<?php get_footer(); ?>
