d1f1a118cf7c9fea41dc294ea2315ac0809a619c
rmalave <rafomalave@gmail.com>
2015-08-05 18:53:45 -0700

    changes to sidebar structure
    made to be more dynamic with a custom plugin added. also changes made to
    search bar

--- single.php
+++ single.php
@@ -9,10 +9,10 @@
 			<!-- the loop -->
 				<?php while (have_posts()) : the_post(); ?>
 					<article id="post-content-<?php the_ID(); ?>" class="post-content">
-						<!--<div class="col-xs-12 col-sm-8">
+						<div class="col-xs-12 col-sm-8">
 							<?php if(has_post_thumbnail()){the_post_thumbnail('',
 								array('class' => "img-responsive center-block"));} ?>
-						</div>-->
+						</div>
 						<div class="col-xs-12">
 							<h1><?php the_title(); ?></h1>
 								<?php the_content();?>
